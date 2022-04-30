2ce30a0035cba60db409fb20cae6a13f245babec
Ruman Sukhera <92282309+Rumam786@users.noreply.github.com>
2022-04-30 22:57:16 +0500

    Add files via upload

---
+++ family.js
@@ -0,0 +1,11 @@
+// let
+let firstName = "Hello World";
+//let firstName = "Hello World";
+firstName = "Heyy"
+document.write(firstName);
+
+// const
+const secondName = "Ruman";
+//secondName = "Rumanhere";
+//const secondName = "usman";
+document.write(secondName);
